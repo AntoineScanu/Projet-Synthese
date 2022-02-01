3b59e0fcf56442911c4b685c73725b22b6c7e6f6
AntoineScanu <antoine.scanu1@gmail.com>
2022-02-01 16:33:32 +0100

    Canevas
    squelette

---
+++ canevas/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "util.h"
+#include "list.h"
+#include "tree.h"
+#include "heap.h"
+#include "sort.h"
+#include "geometry.h"
+#include "algo.h"
+
+int main() {
+
+	return EXIT_SUCCESS;
+}
