abe8fa88025c2f14359a0291a5e7471ec2e71974
Valaaz <val.zancoth@gmail.com>
2022-02-01 17:43:33 +0100

    create getters/setters for node

--- canevas/src/list.c
+++ canevas/src/list.c
@@ -8,31 +8,41 @@
  * LNode
  ********************************************************************/
 
-// Construction 
-LNode * newLNode(void* data) {
-	// TODO
+// Construction
+LNode *newLNode(void *data)
+{
+	struct ListNode *L = (struct ListNode *)calloc(1, sizeof(struct ListNode));
+	assert(data);
+	L->data = data;
+	return L;
 }
 
 // Consultation
-void* getLNodeData(const LNode* node) {
-	// TODO
+void *getLNodeData(const LNode *node)
+{
+	return node->data;
 }
-LNode* Successor(const LNode* node) {
-	// TODO
+LNode *Successor(const LNode *node)
+{
+	return node->succ;
 }
-LNode* Predecessor(const LNode* node) {
-	// TODO
+LNode *Predecessor(const LNode *node)
+{
+	return node->pred;
 }
 
 // Modification
-void setLNodeData(LNode* node, void* newData) {
-	// TODO
+void setLNodeData(LNode *node, void *newData)
+{
+	node->data = newData;
 }
-void setSuccessor(LNode* node, LNode* newSucc) {
-	// TODO
+void setSuccessor(LNode *node, LNode *newSucc)
+{
+	node->succ = newSucc;
 }
-void setPredecessor(LNode* node, LNode* newPred) {
-	// TODO
+void setPredecessor(LNode *node, LNode *newPred)
+{
+	node->pred = newPred;
 }
 
 /********************************************************************
@@ -40,85 +50,106 @@ void setPredecessor(LNode* node, LNode* newPred) {
  ********************************************************************/
 
 // Construction
-List * newList(void (*viewData)(const void*), void (*freeData)(void*)) {
+List *newList(void (*viewData)(const void *), void (*freeData)(void *))
+{
 	// TODO
 }
 
-
-int listIsEmpty(List* L) {
+int listIsEmpty(List *L)
+{
 	assert(L);
-	if (L->numelm ==0){
+	if (L->numelm == 0)
+	{
 		return 0;
 	}
-	else{
+	else
+	{
 		return 1;
 	}
 }
 
 // Consultation
-int getListSize(const List* L) {
+int getListSize(const List *L)
+{
 	return L->numelm;
 }
-LNode* Head(const List* L) {
+LNode *Head(const List *L)
+{
 	// TODO
 }
-LNode* Tail(const List* L) {
+LNode *Tail(const List *L)
+{
 	// TODO
 }
 
 // Modification
-void increaseListSize(List* L) {
+void increaseListSize(List *L)
+{
 	// TODO
 }
-void decreaseListSize(List* L) {
+void decreaseListSize(List *L)
+{
 	// TODO
 }
-void setListSize(List* L, int newSize) {
+void setListSize(List *L, int newSize)
+{
 	// TODO
 }
-void resetListSize(List* L) {
+void resetListSize(List *L)
+{
 	// TODO
 }
-void setHead(List* L, LNode* newHead) {
+void setHead(List *L, LNode *newHead)
+{
 	// TODO
 }
-void setTail(List* L, LNode* newTail) {
+void setTail(List *L, LNode *newTail)
+{
 	// TODO
 }
 
 // Liberation
-void freeList(List * L, int deleteData) {
+void freeList(List *L, int deleteData)
+{
 	assert(deleteData == 0 || deleteData == 1);
 	// TODO
 }
 
 // Consultation
-void viewList(const List * L) {
+void viewList(const List *L)
+{
 	// TODO
 }
 
 // Modification
-void listInsertFirst(List * L, void * data) {
+void listInsertFirst(List *L, void *data)
+{
 	// TODO
 }
-void listInsertLast(List * L, void * data) {
+void listInsertLast(List *L, void *data)
+{
 	// TODO
 }
-void listInsertAfter(List * L, void * data, LNode * ptrelm) {
+void listInsertAfter(List *L, void *data, LNode *ptrelm)
+{
 	// TODO
 }
-void* listRemoveFirst(List * L) {
+void *listRemoveFirst(List *L)
+{
 	assert(Head(L));
 	// TODO
 }
-void* listRemoveLast(List * L) {
+void *listRemoveLast(List *L)
+{
 	assert(Head(L));
 	// TODO
 }
-void* listRemoveNode(List * L, LNode * node) {
+void *listRemoveNode(List *L, LNode *node)
+{
 	assert(Head(L) && Tail(L));
 	// TODO
 }
-List* listConcatenate(List* L1, List* L2) {
+List *listConcatenate(List *L1, List *L2)
+{
 	// TODO
 }
