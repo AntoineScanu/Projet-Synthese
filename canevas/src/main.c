89db770396b384a3da2a602e0b075bfcad310ac1
Valaaz <val.zancoth@gmail.com>
2022-04-29 22:26:55 +0200

    Update main.c

--- canevas/src/main.c
+++ canevas/src/main.c
@@ -8,18 +8,11 @@
 #include "geometry.h"
 #include "algo.h"
 
-
-int main() {
+int main()
+{
 
     List *L1 = newList(viewInt, freeInt);
     List *L2 = newList(viewInt, freeInt);
-
-    // int *data1 = (int *)calloc(1, sizeof(int));
-    // *data1 = 1;
-    // int *data2 = (int *)calloc(1, sizeof(int));
-    // *data2 = 2;
-    // int *data3 = (int *)calloc(1, sizeof(int));
-    // *data3 = 3;
 
     int ptr1 = 1;
     void *data1 = &ptr1;
@@ -39,22 +32,26 @@ int main() {
     int ptr6 = 6;
     void *data6 = &ptr6;
 
-    listInsertLast(L1, data1);
-    listInsertLast(L1, data2);
-    listInsertLast(L1, data3);
+    listInsertFirst(L1, data1);
+    listInsertFirst(L1, data2);
+    listInsertFirst(L1, data3);
 
     listInsertLast(L2, data4);
     listInsertLast(L2, data5);
     listInsertLast(L2, data6);
 
-    // viewAll(L1);
-    // viewAll(L2);
+    viewAll(L1);
+    viewAll(L2);
+
+    listRemoveFirst(L1);
+    listRemoveLast(L2);
+
+    viewAll(L1);
+    viewAll(L2);
 
     List *L3 = listConcatenate(L1, L2);
 
     viewAll(L3);
-
-    // freeList(L1, 1);
 
     return EXIT_SUCCESS;
 }
